b39383064f4f7041cfe28b031e03e200e3bf299a
Lauren Wiebe <lewiebe2@gmail.com>
2022-01-21 20:23:21 -1000

    Uploading modified app.py

--- app.py
+++ app.py
@@ -24,4 +24,5 @@ def scrape():
    mars.update_one({}, {"$set":mars_data}, upsert=True)
    return redirect('/', code=302)
 
-   mars.update_one({}, {"$set":mars_data}, upsert=True)
+if __name__ == "__main__":
+   app.run()
